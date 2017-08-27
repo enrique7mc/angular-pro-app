c421acae8fe200a603057415cf9f01fefedf9b17
Enrique Munguía <enrique7mc@gmail.com>
2017-08-26 20:30:59 -0700

    add header and nav components

--- src/app/containers/app/app.component.ts
+++ src/app/containers/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
 import { Store } from 'store';
@@ -10,7 +11,10 @@ import { AuthService, User } from '../../../auth/shared/services/auth/auth.servi
   styleUrls: ['app.component.scss'],
   template: `
     <div>
-      <h1>{{ user$ | async | json }}</h1>
+      <app-header [user]="user$ | async" (logout)="onLogout()">
+      </app-header>
+      <app-nav *ngIf="(user$ | async)?.authenticated">
+      </app-nav>
       <div class="wrapper">
         <router-outlet></router-outlet>
       </div>
@@ -24,6 +28,7 @@ export class AppComponent implements OnInit, OnDestroy {
 
   constructor(
     private authService: AuthService,
+    private router: Router,
     private store: Store
   ) {}
 
@@ -34,5 +39,10 @@ export class AppComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
+  }
+
+  async onLogout() {
+    await this.authService.logoutUser();
+    this.router.navigate(['/auth/login']);
   }
 }
