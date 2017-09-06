e999cb43cad4af6c0b0956b4d4285d96510d457b
Enrique Munguía <enrique7mc@gmail.com>
2017-09-06 07:44:38 -0700

    grab key params to fill in meal component

--- src/health/meals/containers/meal/meal.component.ts
+++ src/health/meals/containers/meal/meal.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+import 'rxjs/add/operator/switchMap';
+
 import { Meal, MealsService } from '../../../shared/services/meals/meals.service';
 
 @Component({
@@ -10,7 +15,11 @@ import { Meal, MealsService } from '../../../shared/services/meals/meals.service
 			<div class="meal__title">
 				<h1>
 					<img src="/img/food.svg" alt="food">
-					<span>Create meal</span>
+					<span *ngIf="meal$ | async as meal; else title;">
+            {{ meal.name ? 'Edit' : 'Create' }} meal
+          </span>
+          <ng-template #title>
+          </ng-template>
 				</h1>
 			</div>
 			<div>
@@ -19,11 +28,26 @@ import { Meal, MealsService } from '../../../shared/services/meals/meals.service
 		</div>
 	`
 })
-export class MealComponent {
+export class MealComponent implements OnInit, OnDestroy {
+
+  meal$: Observable<Meal>;
+  subscription: Subscription;
+
 	constructor(
     private mealsService: MealsService,
-    private router: Router
+    private router: Router,
+    private route: ActivatedRoute
   ) {}
+
+  ngOnInit() {
+    this.subscription = this.mealsService.meals$.subscribe();
+    this.meal$ = this.route.params
+      .switchMap(param => this.mealsService.getMeal(param.id));
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 
 	async addMeal(event: Meal) {
 		await this.mealsService.addMeal(event);
